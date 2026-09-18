06b58220dbd617d2918b5584f4cd55fc37e6030e
Michaelpeatross <Michaelpeatross@gmail.com>
2026-09-18 15:22:29 -0500

    Make OG image assembler safe if part files are not present yet

--- og_food_image.py
+++ og_food_image.py
@@ -1,6 +1,9 @@
-"""Embedded 1200x630 JPEG assembled from og_food_p1..p4."""
-from og_food_p1 import P1
-from og_food_p2 import P2
-from og_food_p3 import P3
-from og_food_p4 import P4
-OG_FOOD_JPG_B64 = "".join((P1, P2, P3, P4))
+"""Embedded 1200x630 JPEG for /static/og-food-scanner.png."""
+try:
+    from og_food_p1 import P1
+    from og_food_p2 import P2
+    from og_food_p3 import P3
+    from og_food_p4 import P4
+    OG_FOOD_JPG_B64 = "".join((P1, P2, P3, P4))
+except Exception:
+    OG_FOOD_JPG_B64 = ""
