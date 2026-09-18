d9d1b727493538828a4f62e0484bb78efcaff3cd
Michaelpeatross <Michaelpeatross@gmail.com>
2026-09-18 15:20:51 -0500

    Add OG image assembler that loads the attached Food Scanner card

---
+++ og_food_image.py
@@ -0,0 +1,6 @@
+"""Embedded 1200x630 JPEG assembled from og_food_p1..p4."""
+from og_food_p1 import P1
+from og_food_p2 import P2
+from og_food_p3 import P3
+from og_food_p4 import P4
+OG_FOOD_JPG_B64 = "".join((P1, P2, P3, P4))
